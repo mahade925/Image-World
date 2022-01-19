b8f585a1c79451f0a7b7102d12c2cc17b24e0439
mahade925 <learnwithhasan2@gmail.com>
2022-01-19 18:48:14 +0600

    user page created

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/index.module.css";
+import Head from "next/head";
+import { useState } from "react";
 
 export const getStaticProps = async () => {
+
   const res = await fetch("https://api.unsplash.com/photos/?client_id=RgbvYAobryBKphBsjFcebaRUtgIB0497YfO7EJmBdms");
   const data = await res.json();
 
@@ -14,10 +17,21 @@ export const getStaticProps = async () => {
 };
 
 const index = ({ data }) => {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h1 className={styles.indexTitle}>Image World</h1>
-      {console.log(data)}
+      <Head>
+        <title>Image World</title>
+        <link rel="shortcut icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXJqQKZY-TvCsSOSrBpXpm6vZU9J8_vqY4HA&usqp=CAU" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1 className={styles.indexTitle}>Image World {count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        +
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -
+      </button>
       <div className={styles.imageContainer}>
         {
           data.map((curElm) => {
---
+++ pages/users/index.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+
+const users = () => {
+    const [users, setUsers] = useState([]);
+    const [moreUsers, setMoreUsers] = useState(5);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data.slice(0,moreUsers)))
+    }, [moreUsers]);
+
+    console.log(users)
+
+    return (
+        <>
+            <Head>
+                <title>Users</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+                <link rel="shortcut icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwnYnwftDUSjsQmLQvMBZ2pwDXhAJiIdfKvg&usqp=CAU" />
+            </Head>
+            <h1 className="text-center mt-5">Hello users - {moreUsers}</h1>
+            <div className="row row-cols-1 row-cols-md-3 g-4 container m-auto">
+            {
+                users.map(user => <>
+                        <div className="col">
+                            <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{user.name}</h5>
+                                        <h6>Email : {user.email}</h6>
+                                        <h6>Phone : {user.phone}</h6>
+                                        {/* <h6>Address : {user.address.city}</h6> */}
+                                        <button type="button" class="btn btn-info">View Detail</button>
+                                    </div>
+                            </div>
+                        </div>
+                </>)
+            }
+            </div>
+            {
+                moreUsers==5 ? <div className="text-center mt-3">
+                <button onClick={() => setMoreUsers(moreUsers+5)} type="button" className="text-center btn btn-primary">Show More</button>
+                </div> : <div className="text-center mt-3">
+                <button onClick={() => setMoreUsers(moreUsers-5)} type="button" className="btn btn-danger">Show less</button>
+                </div>
+            }
+        </>
+    )
+}
+
+export default users
